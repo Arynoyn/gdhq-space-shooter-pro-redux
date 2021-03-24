1217fe33741fe93bbaa1a9f78386355bc74e5d0f
Christopher West (Arynoyn) <Arynoyn@gmail.com>
2021-03-23 20:16:18 -0600

    Added player movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField]
+    private float _speed = 3.5f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,10 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
         
+        transform.Translate(Vector3.right * (horizontalInput * _speed * Time.deltaTime));
+        transform.Translate(Vector3.up * (verticalInput * _speed * Time.deltaTime));
     }
 }
