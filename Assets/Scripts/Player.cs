77062f777f1d7530fafe31fbff89d3c5385c84b3
Christopher West (Arynoyn) <Arynoyn@gmail.com>
2021-04-01 10:27:53 -0500

    Converted from Old Input Manager to Unity's new Input System

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class Player : MonoBehaviour
+public class Player : MonoBehaviour, Controls.IPlayerActions
 {
     [SerializeField]
     private float _speed = 3.5f;
+    private Vector3 _direction;
 
     // Start is called before the first frame update
     void Start()
@@ -16,10 +18,16 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-        
-        transform.Translate(Vector3.right * (horizontalInput * _speed * Time.deltaTime));
-        transform.Translate(Vector3.up * (verticalInput * _speed * Time.deltaTime));
+        transform.Translate(_direction * (_speed * Time.deltaTime));
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        _direction = context.ReadValue<Vector2>();
+    }
+
+    public void OnFire(InputAction.CallbackContext context)
+    {
+        throw new System.NotImplementedException();
     }
 }
