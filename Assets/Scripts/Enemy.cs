601cace4b3abc3840bd025206282042955952527
Christopher West (Arynoyn) <Arynoyn@gmail.com>
2021-04-06 22:14:47 -0500

    Added Collision logic for laser, enemy and player, Implemented Player Lives and damage system.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Enemy : MonoBehaviour
 {
@@ -10,14 +12,7 @@ public class Enemy : MonoBehaviour
     private float _leftScreenLimit = -8f;
     private float _rightScreenLimit = 8f;
     private float _zPos = 0f;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    
     void Update()
     {
         transform.Translate(Vector3.down * (_movementSpeed * Time.deltaTime));
@@ -27,5 +22,25 @@ public class Enemy : MonoBehaviour
             float randomXPos = Random.Range(_leftScreenLimit, _rightScreenLimit);
             transform.position = new Vector3(randomXPos, _topScreenLimit, _zPos);
         }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Player player = other.gameObject.GetComponent<Player>();
+            if (player != null)
+            {
+                player.Damage();
+            }
+            Destroy(gameObject);
+        }
+
+        if (other.CompareTag("Laser"))
+        {
+            Destroy(other.gameObject);
+        }
+        
+        
     }
 }
