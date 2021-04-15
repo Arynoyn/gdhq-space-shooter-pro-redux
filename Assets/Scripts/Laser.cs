f9c1e2cb748c6f857d75523ca25a5d0cb64d2777
Christopher West (Arynoyn) <Arynoyn@gmail.com>
2021-04-14 23:08:25 -0500

    Create triple shot and add code to player to instantiate it when active

--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -20,7 +20,15 @@ public class Laser : MonoBehaviour
         transform.Translate(Vector3.up * (_speed * Time.deltaTime));
         if (transform.position.y > _screenTop)
         {
-            Destroy(gameObject);
+            if (transform.parent != null)
+            {
+                Destroy(transform.parent.gameObject);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+            
         }
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,9 +1,14 @@
+using System.Numerics;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Quaternion = UnityEngine.Quaternion;
+using Vector2 = UnityEngine.Vector2;
+using Vector3 = UnityEngine.Vector3;
 
 public class Player : MonoBehaviour, Controls.IPlayerActions
 {
     [SerializeField] private GameObject _laserPrefab;
+    [SerializeField] private GameObject _tripleShotPrefab;
     [SerializeField] private float _movementSpeed = 3.5f;
     [SerializeField] private float _fireRate = 0.15f;
     [SerializeField] private int _lives = 3;
@@ -20,7 +25,10 @@ public class Player : MonoBehaviour, Controls.IPlayerActions
 
     private Vector3 _laserOffset = new Vector3(0f, 1.0f, 0f);
     private float _nextFire = -1f;
+    [SerializeField] private bool _tripleShotActive = false;
+
     private SpawnManager _spawnManager;
+    
     private bool _isMovingRight;
     private bool _isMovingLeft;
 
@@ -38,6 +46,16 @@ public class Player : MonoBehaviour, Controls.IPlayerActions
         if (_animator == null)
         {
             Debug.LogError("Player Animator in Player class is NULL");
+        }
+        
+        if (_laserPrefab == null)
+        {
+            Debug.LogError("Laser Prefab in Player class is NULL");
+        }
+        
+        if (_tripleShotPrefab == null)
+        {
+            Debug.LogError("Triple Shot Prefab in Player class is NULL");
         }
     }
 
@@ -77,8 +95,10 @@ public class Player : MonoBehaviour, Controls.IPlayerActions
         if (context.started && Time.time > _nextFire)
         {
             _nextFire = Time.time + _fireRate;
-            Vector3 laserSpawnOffset = transform.position + _laserOffset;
-            Instantiate(_laserPrefab, laserSpawnOffset, Quaternion.identity);
+            Vector3 playerPosition = transform.position;
+            Vector3 laserSpawnOffset = _tripleShotActive ? playerPosition : playerPosition + _laserOffset;
+            GameObject shotPrefab = _tripleShotActive ? _tripleShotPrefab : _laserPrefab;
+            Instantiate(shotPrefab, laserSpawnOffset, Quaternion.identity);
         }
     }
 
