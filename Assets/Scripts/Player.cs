fba5cfe47ea266e8145196f975373076a8d1b108
Chris West <Arynoyn@gmail.com>
2022-04-30 20:47:49 -0500

    Add player script and set initial position
    Add player script and set initial position

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.transform.position = new Vector2(0f,-3.9f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
