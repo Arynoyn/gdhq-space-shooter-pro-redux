e8402f689b8b31c809c9de83fcbc0821b44a58e7
Chris West <Arynoyn@gmail.com>
2022-04-30 21:33:57 -0500

    Added Player Movement
    Added Player Movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class Player : MonoBehaviour
+public class Player : MonoBehaviour, Controls.IPlayerActions
 {
+    private Vector2 _direction;
+
+    [SerializeField] private float _speed = 3.5f;
+
     // Start is called before the first frame update
     void Start()
     {
-        this.transform.position = new Vector2(0f,-3.9f);
+        transform.position = new Vector2(0f,-3.9f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(_direction * (_speed * Time.deltaTime));
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        _direction = context.ReadValue<Vector2>();
+    }
+
+    public void OnFire(InputAction.CallbackContext context)
+    {
+        throw new System.NotImplementedException();
     }
 }
