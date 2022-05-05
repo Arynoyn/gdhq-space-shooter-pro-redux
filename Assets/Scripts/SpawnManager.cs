ec1cf427303d5422f6bd1ee3093b5255f6da3bdc
Chris West <Arynoyn@gmail.com>
2022-05-04 21:00:14 -0500

    Add code to spawn enemies from spawn manager
    Add code to spawn enemies from spawn manager

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -1,18 +1,32 @@
+#region
+
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+
+#endregion
 
 public class SpawnManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float _spawnRate = 5.0f;
+    [SerializeField] private GameObject _enemyPrefab;
+    private float _screenLimitLeft = -8f;
+    private float _screenLimitRight = 8f;
+    private float _screenLimitTop = 8.0f;
+    private float _zPos = 0f;
+
+    private void Start()
     {
-        
+        StartCoroutine(SpawnEnemyRoutine());
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator SpawnEnemyRoutine()
     {
-        
+        while (true)
+        {
+            float xPos = Random.Range(_screenLimitLeft, _screenLimitRight);
+            Vector3 spawnPosition = new Vector3(xPos, _screenLimitTop, _zPos);
+            Instantiate(_enemyPrefab, spawnPosition, Quaternion.identity);
+            yield return new WaitForSeconds(_spawnRate);
+        }
     }
 }
