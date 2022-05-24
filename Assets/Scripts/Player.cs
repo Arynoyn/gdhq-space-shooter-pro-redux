1544aa94ae6715d82f4f57aec053a57776463853
Chris West <Arynoyn@gmail.com>
2022-05-24 17:48:52 -0500

    Added UI for Player lives
    Added UI for Player lives

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -89,7 +89,8 @@ public class Player : MonoBehaviour, Controls.IPlayerActions
         
         transform.position = new Vector3(_horizontalStartPosition, _verticalStartPosition, _zPos);
         _score = 0;
-        _uiManager.SetScoreText(_score);
+        _uiManager.SetScore(_score);
+        _uiManager.SetLives(_lives);
     }
 
     void Update()
@@ -146,6 +147,7 @@ public class Player : MonoBehaviour, Controls.IPlayerActions
         else
         {
             _lives--;
+            _uiManager.SetLives(_lives);
             if (_lives < 1)
             {
                 _spawnManager.StopSpawningEnemies();
@@ -181,7 +183,7 @@ public class Player : MonoBehaviour, Controls.IPlayerActions
     public void IncreaseScore(int value)
     {
         _score += value;
-        _uiManager.SetScoreText(_score);
+        _uiManager.SetScore(_score);
     }
     
     IEnumerator TripleShotCooldownRoutine()
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -4,9 +4,16 @@ using UnityEngine.UI;
 public class UIManager : MonoBehaviour
 {
     [SerializeField] private Text _scoreText;
+    [SerializeField] private Sprite[] _livesImages;
+    [SerializeField] private Image _livesDisplay;
 
-    public void SetScoreText(int score)
+    public void SetScore(int score)
     {
         _scoreText.text = score.ToString();
+    }
+    
+    public void SetLives(int lives)
+    {
+        _livesDisplay.sprite = _livesImages[lives];
     }
 }
