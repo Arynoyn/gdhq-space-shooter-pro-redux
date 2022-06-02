2c28cba2c01aad46029a30fc17c24cfdbed11d5f
Chris West <Arynoyn@gmail.com>
2022-06-02 11:57:09 -0500

    Added audio clip on laser instantiation
    Added audio clip on laser instantiation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,7 +5,7 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour, Controls.IPlayerActions
 {
-    //Player Properties
+    // Player Properties
     [SerializeField] private int _lives = 3;
     [SerializeField] private int _score;
     
@@ -23,7 +23,7 @@ public class Player : MonoBehaviour, Controls.IPlayerActions
     private float _rightMovementLimit = 9.5f;
     private float _zPos = 0f;
 
-    //Projectile Properties
+    // Projectile Properties
     [SerializeField] private float _fireRate = 0.15f;
     [SerializeField] private GameObject _laserPrefab;
     [SerializeField] private GameObject _tripleShotPrefab;
@@ -31,11 +31,11 @@ public class Player : MonoBehaviour, Controls.IPlayerActions
     private Vector3 _laserOffset = new Vector3(0f, 1.25f, 0f);
     private float _nextFire = -1f;
     
-    //Speed Boost Properties
+    // Speed Boost Properties
     [SerializeField] private float _speedBoostModifier = 2.0f;
     private bool _speedBoostActive;
     
-    //Shield Properties
+    // Shield Properties
     [SerializeField] private int _maxShieldStrength = 3;
     private bool _shieldsActive;
     private int _shieldStrength;
@@ -47,6 +47,10 @@ public class Player : MonoBehaviour, Controls.IPlayerActions
     private GameObject _leftEngineDamageVisualizer;
     private static readonly int IsTurningLeft = Animator.StringToHash("isTurningLeft");
     private static readonly int IsTurningRight = Animator.StringToHash("isTurningRight");
+    
+    // Audio
+    [SerializeField] private AudioClip _laserSound;
+    private AudioSource _audioSource;
 
     void Start()
     {
@@ -102,6 +106,12 @@ public class Player : MonoBehaviour, Controls.IPlayerActions
             Debug.LogError("Triple Shot Prefab in Player class is NULL");
         }
         
+        _audioSource = GetComponent<AudioSource>();
+        if (_audioSource == null)
+        {
+            Debug.LogError("AudioSource in Player class is NULL");
+        }
+        
         transform.position = new Vector3(_horizontalStartPosition, _verticalStartPosition, _zPos);
         _score = 0;
         _gameManager.SetScore(_score);
@@ -148,6 +158,7 @@ public class Player : MonoBehaviour, Controls.IPlayerActions
             Vector3 laserSpawnOffset = _tripleShotActive ? playerPosition : playerPosition + _laserOffset;
             GameObject shotPrefab = _tripleShotActive ? _tripleShotPrefab : _laserPrefab;
             Instantiate(shotPrefab, laserSpawnOffset, Quaternion.identity);
+            if (_audioSource != null) { _audioSource.PlayOneShot(_laserSound); }
         }
     }
     
