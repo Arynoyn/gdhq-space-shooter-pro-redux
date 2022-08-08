77b69a358f3517bb675fc7cd4ec967bee91ecf78
Chris West <Arynoyn@gmail.com>
2022-08-08 16:48:20 -0500

    Added Scrolling Credits Scene
    Added Scrolling Credits Scene

--- Assets/Scripts/WinScreen/WinScreen.cs
+++ Assets/Scripts/WinScreen/WinScreen.cs
@@ -1,10 +1,18 @@
+using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class WinScreen : MonoBehaviour
 {
-    public void BackToMainMenu()
+    public void Start()
     {
+        StartCoroutine(ReturnToMainMenuRoutine());
+    }
+
+    IEnumerator ReturnToMainMenuRoutine()
+    {
+        yield return new WaitForSeconds(32);
         SceneManager.LoadScene("Main_Menu");
     }
 }
